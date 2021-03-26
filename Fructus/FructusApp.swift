2515f433ee12c43f8424ab6efea534fe54aed66c
Azhar Islam <azhartislam@gmail.com>
2021-03-26 15:07:48 +0000

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Azhar on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
