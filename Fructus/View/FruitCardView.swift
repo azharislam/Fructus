885673c3b69ae62895062ce938f25a3219e80eb9
Azhar Islam <azhartislam@gmail.com>
2021-03-26 16:41:56 +0000

    Added onboarding

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct FruitCardView: View {
     
     // MARK: - PROPERTIES
+    @State private var isAnimating: Bool = false
     
     // MARK: - BODY
-    
-    
     var body: some View {
         ZStack {
             VStack(spacing: 20) {
@@ -29,6 +28,7 @@ struct FruitCardView: View {
                     .font(.largeTitle)
                     .fontWeight(.heavy)
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                    .scaleEffect(isAnimating ? 1.0 : 0.6)
                 
                 // Fruit: Headline
                 Text("Blueberries are sweet, nutritious and a wildly popular fruit all over the world.")
@@ -41,9 +41,15 @@ struct FruitCardView: View {
                 StartButtonView()
             } //: VSTACK
         } //: ZSTACK
+        .onAppear {
+            withAnimation(.easeOut(duration: 0.5)) {
+                isAnimating = true
+            }
+        }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: .infinity, alignment: .center)
         .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
         .cornerRadius(20)
+        .padding(.horizontal, 20)
     }
 }
 
